226571a8edcf18bc418fefac4f05bbb14103a569
VaishaliChouhan <vaishali@gmail.com>
2024-12-27 19:04:52 +0530

    code done

---
+++ vastcalc/vastcalc/vaishali-code/average1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+n3 = int(input("Enter third number: "))
+
+average = (n1+n2+n3)/3
+
+print("average of three number is: " , average)
