b450a4db4e03c33ad183ed59f51b0dfb1d70a37d
pallavi_bajpai <pallavibajpai@gmail.com>
2024-12-27 17:41:41 +0530

    code is implimented

---
+++ vastcalc/vastcalc/palak-code/palak.py
@@ -0,0 +1,8 @@
+math=float(input("enter the math "))
+english=float(input("enter the english "))
+hindi=float(input("enter the hindi "))
+physics=float(input("enter the physics "))
+chemistry=float(input("enter the chemistry "))
+total=math+english+hindi+physics+chemistry
+percentage=total*100/500
+print("percentages of the total marks" , percentage)
