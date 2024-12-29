3bcf51c0f8be001c3c8d5cf2abb0bcd9f938b85e
ayush-9977 <ayush@gmail.com>
2024-12-29 19:01:36 +0530

    addition

---
+++ vastcalc/vastcalc/Ayush-code/sub_add.py
@@ -0,0 +1,7 @@
+def addition(n1,n2):
+    sum =(n1+n2)
+    return sum
+num1 = int(input("Enter a first number :"))
+num2 = int(input("Enter a first number :"))
+result = addition(num1,num2)
+print("addition of two number : ",result)
