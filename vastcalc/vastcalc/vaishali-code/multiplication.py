cb0ad0fa414108f9085131a0fc18a9a7cdbc71dd
VaishaliChouhan <vaishali@gmail.com>
2024-12-30 16:01:36 +0530

    division code

--- vastcalc/vastcalc/vaishali-code/multiplication.py
+++ vastcalc/vastcalc/vaishali-code/multiplication.py
@@ -1,6 +1,6 @@
 
-num1 = int(input("Enter first number "))
-num2 = int(input("Enter second number"))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 multi = num1*num2
 
