65bf44ff173e00596d32c599eded55e857091d8a
Umeshyadav21 <eng.yadavumesh@gmail.com>
2024-12-30 19:02:38 +0530

    This is the percentage program

---
+++ vastcalc/vastcalc/Persentage.py
@@ -0,0 +1,10 @@
+
+def percentage(a, b):
+    return (a*b)/100
+
+input1= int(input("Enter total number: "))
+input2= int(input("Enter the percentage: ")) 
+
+result = percentage(input2, input1)
+
+print("The percentage of the two numbers is: ", result)
