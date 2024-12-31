2da5c3540451e8a61f37882afcce727a2e3ed51d
VaishaliChouhan <vaishali@gmail.com>
2024-12-31 15:11:50 +0530

    division_function

---
+++ vastcalc/vastcalc/vaishali-code/division_fun.py
@@ -0,0 +1,10 @@
+
+
+def division(n1,n2):
+    div = n1*n2
+    return sum
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = division(num1,num2)
+print("division of two number is: ", result)
