401f2b3e72002db72bb21d6ab8077ee28eb0c695
VaishaliChouhan <vaishali@gmail.com>
2024-12-31 15:03:01 +0530

    substraction_function

---
+++ vastcalc/vastcalc/vaishali-code/substraction_fun.py
@@ -0,0 +1,9 @@
+
+def addition(n1,n2):
+    sub = n1-n2
+    return sub
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = substraction(num1,num2)
+print("substraction of two number is: ", result)
