a61cbaf7840debe037edde50a6f2f9f9a1de143d
VaishaliChouhan <vaishali@gmail.com>
2024-12-31 14:45:35 +0530

    addition_function

---
+++ vastcalc/vastcalc/vaishali-code/Addition_fun.py
@@ -0,0 +1,9 @@
+
+def addition(n1,n2):
+    sum = n1+n2
+    return sum
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result =addition(num1,num2)
+print("addition of two number is: ", result)
