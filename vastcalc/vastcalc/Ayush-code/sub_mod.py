2d111a34dbb632a231be7f4967faabd148b41013
ayush-9977 <ayush@gmail.com>
2025-01-01 17:10:41 +0530

    mod

--- vastcalc/vastcalc/Ayush-code/sub_mod.py
+++ vastcalc/vastcalc/Ayush-code/sub_mod.py
@@ -4,4 +4,4 @@ def modulus(n1,n2):
 num1= int(input("Enter first number: "))
 num2= int(input("Enter second number: "))
 result= modulus(num1,num2)
-print("division of two number : ",result)
+print("modulus of two number : ",result)
