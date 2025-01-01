856f7b9bdf3d28cd61cc238fcbfa0cb750762637
ayush-9977 <ayush@gmail.com>
2025-01-01 16:06:51 +0530

    sub two no.

---
+++ vastcalc/vastcalc/Ayush-code/sub_sub.py
@@ -0,0 +1,7 @@
+def subtraction(n1,n2):
+    sub =(n1-n2)
+    return sub
+num1 = int(input("Enter a first number :"))
+num2 = int(input("Enter a first number :"))
+result = subtraction(num1,num2)
+print("subtraction of two number : ",result)
