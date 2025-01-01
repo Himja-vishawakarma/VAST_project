a9486a93ee366ebd55656276699015b82ffc3aa1
ayush-9977 <ayush@gmail.com>
2025-01-01 16:31:39 +0530

    division

---
+++ vastcalc/vastcalc/Ayush-code/sub_div.py
@@ -0,0 +1,7 @@
+def division(n1,n2):
+    div =(n1//n2)
+    return div
+num1 = int(input("Enter a first number :"))
+num2 = int(input("Enter a first number :"))
+result = division(num1,num2)
+print("division of two number : ",result)
