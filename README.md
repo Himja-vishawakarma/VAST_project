e1bf251f90d096724601743ffe9f888dc465b863
Iamyashjain <yashjain200502@gmail.com>
2025-01-03 01:29:00 +0530

    DockerFile addedd

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VAST_project
+
 This repository is for vast project Calculator
+
+# Dependency are mentioned in Requirments.txt
+
+# Django version should be 4.2
--- vastcalc/myenv/Scripts/activate.bat
+++ vastcalc/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ADMIN\Desktop\VAST project\myenv
+set VIRTUAL_ENV=E:\VAST\VAST26_12 GitHub\VAST_project\vastcalc\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
