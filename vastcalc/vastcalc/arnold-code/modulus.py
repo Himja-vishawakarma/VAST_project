71ca8a424ac035803e845d9e04334b214644f511
Arnoldreji <arnoldreji25008@gmail>
2025-01-02 11:13:59 +0530

    modulus function

---
+++ vastcalc/vastcalc/arnold-code/modulus.py
@@ -0,0 +1,11 @@
+# Function to calculate the modulus of two numbers
+def calculate_modulus(a, b):
+    return a % b
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calling the function and printing the result
+result = calculate_modulus(num1, num2)
+print(f"The modulus of {num1} and {num2} is {result}\n")
