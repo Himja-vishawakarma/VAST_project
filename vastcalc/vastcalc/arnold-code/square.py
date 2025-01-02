aa49d4081f3c2b8fc6057ad87c2d86760cf121b8
Arnoldreji <arnoldreji25008@gmail>
2025-01-02 11:11:17 +0530

    square function

---
+++ vastcalc/vastcalc/arnold-code/square.py
@@ -0,0 +1,10 @@
+# Function to calculate the square of a number
+def square_number(a):
+    return a * a
+
+# Taking input from the user
+num = float(input("Enter a number to square: "))
+
+# Calling the function and printing the result
+result = square_number(num)
+print(f"The square of {num} is {result}\n")
