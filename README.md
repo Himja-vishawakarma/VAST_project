12597742405b1afcbdc8e3b2e962f85bc6c6da0a
pallavi_bajpai <pallavibajpai@gmail.com>
2025-01-03 14:22:25 +0530

    added function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VAST_project
 This repository is for vast project Calculator
+
+#  Django version should be 4.2
+
---
+++ vastcalc/vastcalc/pallavi-code/pallavi a_addition.py
@@ -0,0 +1,13 @@
+def calculate_sum(a, b, c):
+    return a + b + c
+
+
+a = float(input("Enter the number a = "))
+b = float(input("Enter the number b = "))
+c = float(input("Enter the number c = "))
+
+sum = calculate_sum(a, b, c)
+
+
+print("Sum of three numbers =", sum)
+
