e472f44a537c0a90a9a8903e9e509f24aacdb3bb
pallavi_bajpai <pallavibajpai@gmail.com>
2025-01-03 15:15:53 +0530

    implemented try,except and finally code

--- vastcalc/vastcalc/pallavi-code/pallavi a_addition.py
+++ vastcalc/vastcalc/pallavi-code/pallavi a_addition.py
@@ -1,15 +1,16 @@
 def calculate_sum(a, b, c):
     return a + b + c
 
-
-a = float(input("Enter the number a = "))
-b = float(input("Enter the number b = "))
-c = float(input("Enter the number c = "))
-
-sum = calculate_sum(a, b, c)
-
-
-print("Sum of three numbers:  =", sum) 
+try:
+    a = float(input("Enter the number a = "))
+    b = float(input("Enter the number b = "))
+    c = float(input("Enter the number c = "))
+    sum = calculate_sum(a, b, c)
+    print("Sum of three numbers =", sum)
+except :
+    print("enter numeric values.")
+finally:
+    print("Program executed successfully.")
 
 
 
