541858be47cd89a71a75b80badf1014fce0c6efd
ayush-9977 <ayush@gmail.com>
2025-01-04 17:07:05 +0530

    persentage

---
+++ vastcalc/vastcalc/Ayush-code/sub_per.py
@@ -0,0 +1,7 @@
+def persentage(n1):
+    per =(n1/500)*(100)
+    return per
+num1 = float(input("Enter a Obtained Value number :"))
+print("Total no. : 500")
+result = persentage(num1)
+print("persentage :",result)
