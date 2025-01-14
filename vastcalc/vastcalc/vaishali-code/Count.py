080071689d5baa9b7a29b393fd24733916bffbec
VaishaliChouhan <vaishali@gmail.com>
2025-01-14 14:42:59 +0530

    methods

---
+++ vastcalc/vastcalc/vaishali-code/Count.py
@@ -0,0 +1,35 @@
+
+#count Method
+
+def vaishaliappend(input,input1):
+    count = 0
+    for i in input:
+        if i == input1:
+            count += 1
+    return count
+
+input = str(input("Enter the name: "))
+input1 = "i"
+print(vaishaliappend(input,input1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+        
+    
+
+
+
---
+++ vastcalc/vastcalc/vaishali-code/pop.py
@@ -0,0 +1,12 @@
+
+
+#pop Method
+
+def vaishaliappend(input):
+    for i in input:
+        input = input[:-1]
+        return input
+    
+
+input = "vaishali"
+print(vaishaliappend(input))
