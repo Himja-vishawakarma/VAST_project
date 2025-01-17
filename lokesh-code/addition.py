6a60d0055c39e709c215d88ceee4cf88f2b40918
LOKESH1617439 <lokeshjaiswal074@gmail.com>
2025-01-17 15:13:12 +0530

    pushing lokesh-code folder

---
+++ lokesh-code/addition.py
@@ -0,0 +1,18 @@
+def addition():
+    
+    try:
+      
+      num1 = int(input("Enter first number: "))
+      num2 = int(input("Enter second number: "))
+
+      result = num1 + num2
+
+      print("addition of two number is: ", result)
+
+    except:
+       print("Invalid input,plzz enter numeric values: ")
+
+    finally:
+       print("addition operation is completed")
+       
+addition()
